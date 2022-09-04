722991cef91be7bf2be8ac8711ac4698ed488f5e
seba7788dev <seby77_88@hotmail.com>
2022-09-04 01:28:58 -0300

    config iniciales de databinding

--- app/src/main/java/com/example/EarthquakeMonitor/MainActivity.kt
+++ app/src/main/java/com/example/EarthquakeMonitor/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.myapplication
+package com.example.EarthquakeMonitor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
